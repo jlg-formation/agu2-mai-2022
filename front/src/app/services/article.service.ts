29a8599a1ea18fd05295f2b4884a79099cc31124
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-24 11:44:53 +0200

    dev: added localstorage

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,31 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
 
+const ARTICLE_KEY = 'articles';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    {
-      name: 'Marteau',
-      price: 2.99,
-      qty: 100,
-    },
-    {
-      name: 'Faucille',
-      price: 5.45,
-      qty: 50,
-    },
-    {
-      name: 'Perceuse',
-      price: 25,
-      qty: 15,
-    },
-  ];
+  articles: Article[] = this.getArticles();
 
   constructor() {}
 
   async add(article: Article) {
     this.articles.push(article);
+    this.save();
+  }
+
+  getArticles(): Article[] {
+    const str = localStorage.getItem(ARTICLE_KEY);
+    if (str === null) {
+      return [
+        {
+          name: 'Marteau',
+          price: 2.99,
+          qty: 100,
+        },
+        {
+          name: 'Faucille',
+          price: 5.45,
+          qty: 50,
+        },
+        {
+          name: 'Perceuse',
+          price: 25,
+          qty: 15,
+        },
+      ];
+    }
+    return JSON.parse(str);
+  }
+
+  save() {
+    localStorage.setItem(ARTICLE_KEY, JSON.stringify(this.articles));
   }
 }

9cc0d25057d472f8acf6b0fdc52ddb65d05c7645
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-24 16:33:37 +0200

    dev: expose GET /api/articles

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
+const articles = [];
+
 const app = Router();
 
 app.get("/date", (req, res) => {
   res.json({ date: new Date() });
+});
+
+app.get("/articles", (req, res) => {
+  res.json(articles);
 });
 
 export const api = app;

88d94854ce2376076574f7868d73066b023250a2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-25 15:07:46 +0200

    test: service article and localstorage

--- front/src/app/services/article.service.spec.ts
+++ front/src/app/services/article.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ArticleService } from './article.service';
+import { ArticleService, ARTICLE_KEY } from './article.service';
 
 describe('ArticleService', () => {
   let service: ArticleService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ArticleService);
   });
 
-  it('should be created', () => {
+  it('should be created with localstorage', () => {
+    localStorage.setItem(ARTICLE_KEY, '[]');
+    service = TestBed.inject(ArticleService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should be created without localstorage', () => {
+    localStorage.clear();
+    service = TestBed.inject(ArticleService);
     expect(service).toBeTruthy();
   });
 });
--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
 
-const ARTICLE_KEY = 'articles';
+export const ARTICLE_KEY = 'articles';
 
 @Injectable({
   providedIn: 'root',

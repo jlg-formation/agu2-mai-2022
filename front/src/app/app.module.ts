002d3a9de0a771b3935de974dc71b6997db873b2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-25 10:24:21 +0200

    dev: added pipe currency with default LOCALE_ID

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
@@ -10,10 +11,16 @@ import { LegalComponent } from './routes/legal/legal.component';
 import { ArticleService } from './services/article.service';
 import { HttpArticleService } from './services/http-article.service';
 
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, HttpClientModule],
-  providers: [{ provide: ArticleService, useClass: HttpArticleService }],
+  providers: [
+    { provide: ArticleService, useClass: HttpArticleService },
+    { provide: LOCALE_ID, useValue: 'fr' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

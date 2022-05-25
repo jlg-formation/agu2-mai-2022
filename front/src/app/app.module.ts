ed78e6f8dc7a0ca906f98cfb36780958abb3e4a4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-25 10:30:43 +0200

    dev: added algerian locale

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import localeFr from '@angular/common/locales/fr';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import localeFr from '@angular/common/locales/fr-DZ';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,14 +12,15 @@ import { ArticleService } from './services/article.service';
 import { HttpArticleService } from './services/http-article.service';
 
 // the second parameter 'fr' is optional
-registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeFr, 'fr-DZ');
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, HttpClientModule],
   providers: [
     { provide: ArticleService, useClass: HttpArticleService },
-    { provide: LOCALE_ID, useValue: 'fr' },
+    { provide: LOCALE_ID, useValue: 'fr-DZ' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'DZD' },
   ],
   bootstrap: [AppComponent],
 })

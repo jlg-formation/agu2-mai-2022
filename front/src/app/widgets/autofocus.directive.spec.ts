7c5c139773f65a5fa378a6188556c031f8f7224a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-25 15:32:14 +0200

    dev: directive

--- front/src/app/widgets/autofocus.directive.spec.ts
+++ front/src/app/widgets/autofocus.directive.spec.ts
@@ -1,6 +1,36 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { AutofocusDirective } from './autofocus.directive';
+
+@Component({
+  selector: 'app-test',
+  template: ` <input appAutofocus /> `,
+})
+export class TestComponent {}
+
+@Component({
+  selector: 'app-test2',
+  template: ` <input appAutofocus="select" /> `,
+})
+export class Test2Component {}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new AutofocusDirective();
-    // expect(directive).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective],
+    }).compileComponents();
+  });
+
+  it('should autofocus', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(document.activeElement).toBe(compiled.querySelector('input'));
+  });
+
+  it('should autofocus', () => {
+    const fixture = TestBed.createComponent(Test2Component);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLInputElement;
   });
 });
